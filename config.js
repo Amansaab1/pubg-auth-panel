5929ed22f0a0c659044a5436a6ec0e553d408aa8
Amansaab1 <128238597+Amansaab1@users.noreply.github.com>
2026-02-14 21:22:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// config.js - Working Configuration
+// config.js - GitHub Configuration
 const GITHUB_CONFIG = {
     gistId: 'e042b686c11cf95f4ffc99fb336df514',
-    filename: 'keys_data.json',
     token: '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'
 };
